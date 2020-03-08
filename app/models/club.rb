4fe730068f779328e02f3c692afc48e8199f822a
Kevin Llewellyn <llewellyn.kevin@yahoo.com>
2020-03-08 17:04:52 -0600

    Added ability to edit a club

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,2 +1,8 @@
-class Club < ApplicationRecord
+class Club < ApplicationRecord 
+  validates :name, 
+    presence: true 
+  validates :pin, 
+    presence: true, 
+    length: { is: 5 },
+    numericality: { only_integer: true }
 end
