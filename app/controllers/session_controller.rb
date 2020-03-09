5a223f04ebab7a6250bfbea401cd1b5debda2ff4
Kevin Llewellyn <llewellyn.kevin@yahoo.com>
2020-03-09 17:31:10 -0600

    Fixed up login forms

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,11 +1,29 @@
 class SessionController < ApplicationController
-  def new
+  def new 
+    @user = User.new
   end
 
-  def create
+  def create 
+    @user = User.find_by(email: params[:email])
+
+    if !@user 
+      @user.errors.add(:base, :invalid, message: "username not found")
+      render 'new'
+    elsif !@user.authenticate(params[:password])
+      @user.errors.add(:base, :invalid, message: "username not found")
+      render 'new'
+    else 
+      session[:user_id] = @user.id 
+      redirect_to '/welcome'
+    end 
   end
 
-  def login
+  def login 
+  end 
+
+  def logout 
+    session.delete(:user_id)
+    redirect_to 'welcome'
   end
 
   def welcome
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'login', to: 'session#new'
   post 'login', to: 'session#create'
+  post 'logout', to: 'session#logout'
   get 'welcome', to: 'session#welcome'
 
   resources :users, only: [:new, :create]
